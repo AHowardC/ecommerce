a268cc717fb150dbe906ea69216c4ba30759ed09
AHowardC <achsvgusaeng@yahoo.com>
2017-12-11 14:08:44 -0500

    nav

---
+++ front-end/src/components/SlickSlider.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class SlickSlider extends Component{
+  render(){
+    return(
+      <h1>Slicker should be here</h1>
+    )
+  }
+}
+
+export default SlickSlider;

66e540859bb3f65ea5e60924b5e63d00447e930c
AHowardC <achsvgusaeng@yahoo.com>
2017-12-12 10:29:30 -0500

    auth, store and redux

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import RootReducer from './reducers/RootReducer';
+import reduxPromise from 'redux-promise';
+import { Provider } from 'react-redux';
+
+const theStore = applyMiddleware(reduxPromise)(createStore)(RootReducer);
+// the friendly way...
+// const middleWare = applyMiddleware(reduxPromise);
+// const storeWithMid = middleWare(createStore);
+// const theStore = storeWithMid(RootReducer);
+
+
 
 ReactDOM.render(
-  <App />,
+  <Provider store={theStore}>
+    <App />
+  </Provider>,
   document.getElementById('root'));
-registerServiceWorker();

ecf20f899aa159d014e522eeff04c329768b84f4
AHowardC <achsvgusaeng@yahoo.com>
2017-12-13 16:04:44 -0500

    ftd

--- front-end/src/containers/NavBar.js
+++ front-end/src/containers/NavBar.js
@@ -6,7 +6,26 @@ class NavBar extends Component{
 	constructor(){
 		super();
 	}
+
+	componentWillReceiveProps(newProps){
+
+	}
+
 	render(){
+		if(this.props.auth.name !== undefined){
+			// the user is logged in
+			var rightMenuBar = [
+				<li className="">Welcome, {this.props.auth.name}</li>,
+				<li><Link to="/cart">(0) items in your cart | ($000)</Link></li>,
+				<li><Link to="/logout">Logout</Link></li>
+			]
+		}else{
+			var rightMenuBar = [
+			    <li><Link to="/login">Sign in</Link> or <Link to="/register">Create an account</Link></li>,
+			    <li>(0) items in cart | ($0.00)</li>
+			]
+		}
+		console.log(this.props.auth);
 		return(
 			<div id="navbar">
 				<nav className="navbar navbar-fixed-top">
@@ -26,8 +45,7 @@ class NavBar extends Component{
 			    				ClassicModels Logo
 			    			</div>
 			    			<div className="nav navbar-nav pull-right">
-			    				<li><Link to="/login">Sign in</Link> or <Link to="/register">Create an account</Link></li>
-			    				<li>(0) items in cart | ($0.00)</li>
+			    				{rightMenuBar}
 			    			</div>
 			    		</div>
 			    	</div>
@@ -37,4 +55,13 @@ class NavBar extends Component{
 	}
 }
 
-export default NavBar;
+
+function mapStateToProps(state){
+	// state = RootReducer
+	return{
+		auth: state.auth
+	}
+}
+
+export default connect(mapStateToProps)(NavBar);
+// export default NavBar;
